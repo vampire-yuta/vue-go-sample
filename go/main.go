d8702e3934228492e01173f47ad32de37d69358b
vampire-yuta <yuta@vamdemicsystem.black>
2022-07-21 14:12:49 +0900

    init

---
+++ go/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Static("/", "dist/")
+
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/hello", hello)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "hello world")
+}
